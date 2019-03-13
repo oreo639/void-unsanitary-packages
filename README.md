ae3ce382f1370afb30b61e03b8f0398181897f23
Oreo639 <31916379+Oreo639@users.noreply.github.com>
2019-03-12 18:13:10 -0700

    Add template manager

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # void-unsanitary-packages
-Just a collection of unoffical templates that are usefull for me, but not really for the main void repo
+Just a collection of unoffical templates that are usefull for me, but not really for the main void repo.
+
+# Usage
+Run `./unsanitary.sh` and then cd into void-packages. Templates are systemlinked from the templates directory into the void-packages directory, so any changes made to the package in the templates directory will affect void-packages.
+
+If you need to update void-packages, just simply rerun `unsanitary.sh`. (WARNING: Any files in the `void-packages/srcpkgs` directory will be deleted.)
---
+++ unsanitary.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SOURCE=$PWD
+
+packages=("devkitpro-pacman")
+
+if [ -z "$SOURCE" ]; then
+	echo '=> Fatal error: Could not get the source directory.'
+	exit 2
+fi
+
+if ! [ -x "$(command -v git)" ]; then
+	echo '=> Please install git to continue. '
+fi
+
+if [ -d "$SOURCE/void-packages" ]; then
+	echo '=> Updating void-packages. '
+	cd $SOURCE/void-packages
+	rm -R $SOURCE/void-packages/srcpkgs/* &>/dev/null
+	git checkout .
+	git pull origin master
+else
+	echo '=> Cloning void-packages. '
+	rm void-packages &>/dev/null
+	git clone https://github.com/void-linux/void-packages $SOURCE/void-packages
+fi
+
+for element in "${packages[@]}"
+do
+	echo "Adding $element..."
+	ln -s $SOURCE/templates/$element $SOURCE/void-packages/srcpkgs/$element 
+done
+
