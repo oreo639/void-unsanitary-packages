ba60b603bf1bb44a32975dd47175e8bebc71a396
Oreo639 <31916379+Oreo639@users.noreply.github.com>
2019-04-03 10:33:32 -0700

    Put quotes around variables

--- unsanitary.sh
+++ unsanitary.sh
@@ -15,19 +15,19 @@ fi
 
 if [ -d "$SOURCE/void-packages" ]; then
 	echo '=> Updating void-packages. '
-	cd $SOURCE/void-packages
-	rm -R $SOURCE/void-packages/srcpkgs/* &>/dev/null
+	cd "$SOURCE/void-packages"
+	rm -R "$SOURCE/void-packages/srcpkgs/*" &>/dev/null
 	git checkout .
 	git pull origin master
 else
 	echo '=> Cloning void-packages. '
 	rm void-packages &>/dev/null
-	git clone https://github.com/void-linux/void-packages $SOURCE/void-packages
+	git clone https://github.com/void-linux/void-packages "$SOURCE/void-packages"
 fi
 
 for element in "${packages[@]}"
 do
 	echo "Adding $element..."
-	cp -r $SOURCE/templates/$element $SOURCE/void-packages/srcpkgs/$element 
+	cp -r "$SOURCE/templates/$element" "$SOURCE/void-packages/srcpkgs/$element" 
 done
 
