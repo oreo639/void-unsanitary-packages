46c729633516eb26559c440cac92f4d5b166bd26
Oreo639 <oreo639@outlook.com>
2019-04-06 19:42:23 -0700

    devkitpro-pacman: Fix obtaining gpg keys and generating nessary folders

--- unsanitary.sh
+++ unsanitary.sh
@@ -5,7 +5,7 @@ SOURCE=$(dirname "$0")
 packages=("devkitpro-pacman")
 
 if [ -z "$SOURCE" ]; then
-	echo '=> Fatal error: Could not get the source directory.'
+	echo '=> Fatal error: Could not get the source directory. '
 	exit 2
 fi
 
@@ -16,11 +16,11 @@ fi
 if [ -d "$SOURCE/void-packages" ]; then
 	echo '=> Updating void-packages. '
 	pushd "$SOURCE/void-packages" &>/dev/null
-	rm -R "$SOURCE/void-packages/srcpkgs/*" &>/dev/null
+	rm -R "$SOURCE/void-packages/srcpkgs/"* &>/dev/null
 	git checkout .
 	git pull origin master
 else
-	echo '=> Cloning void-packages. '
+	echo '=> Cloning void-packages... '
 	rm void-packages &>/dev/null
 	git clone https://github.com/void-linux/void-packages "$SOURCE/void-packages"
 fi
@@ -29,7 +29,7 @@ popd &>/dev/null
 
 for element in "${packages[@]}"
 do
-	echo "Adding $element..."
-	cp -r "$SOURCE/templates/$element" "$SOURCE/void-packages/srcpkgs/" 
+	echo "=> Adding $element... "
+	cp -rp "$SOURCE/templates/$element" "$SOURCE/void-packages/srcpkgs/"
 done
 
